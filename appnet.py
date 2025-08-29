9248db6f9bf9394115f9b88c4530386ba7b6f8f9
namanvarshney76-tech <namanvarshney76@gmail.com>
2025-08-29 16:19:35 +0530

    Update appnet.py

--- appnet.py
+++ appnet.py
@@ -94,7 +94,7 @@ class BigBasketAutomation:
                 flow = Flow.from_client_config(
                     client_config=creds_data,
                     scopes=combined_scopes,
-                    redirect_uri=st.secrets.get("google", {}).get("redirect_uri", "https://bigbasketgrn.streamlit.app/")
+                    redirect_uri=st.secrets.get("google", {}).get("redirect_uri", "https://bbnetgrn.streamlit.app/")
                 )
                 
                 # Generate authorization URL
@@ -1033,4 +1033,5 @@ Duplicate Removal: Based on Item Code + po_number
 
 
 if __name__ == "__main__":
+
     create_streamlit_ui()
