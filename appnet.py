c4d724dd9ecf1edbf469c6037cab7eab758ed96e
namanvarshney76-tech <namanvarshney76@gmail.com>
2025-09-01 15:56:02 +0530

    Update appnet.py

--- appnet.py
+++ appnet.py
@@ -95,7 +95,7 @@ class BigBasketAutomation:
                 flow = Flow.from_client_config(
                     client_config=creds_data,
                     scopes=combined_scopes,
-                    redirect_uri=st.secrets.get("google", {}).get("redirect_uri", "https://bbnetgrn.streamlit.app/")
+                    redirect_uri=st.secrets.get("google", {}).get("redirect_uri", "https://bbnet-auto-grn.streamlit.app/")
                 )
                 
                 # Generate authorization URL
